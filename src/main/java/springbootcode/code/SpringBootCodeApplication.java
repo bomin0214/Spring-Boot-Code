0ea9165ac202498135ea3e085b15654492f53253
bomikim0214 <rlaqhals05@naver.com>
2026-03-27 16:49:14 +0900

    docs: github md 설정

---
+++ src/main/java/springbootcode/code/SpringBootCodeApplication.java
@@ -0,0 +1,13 @@
+package springbootcode.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootcode/code/SpringBootCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootcode.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
