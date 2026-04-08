8e1de3555dc4c09439eecafdbccc1d09988b7413
bomikim0214 <rlaqhals05@naver.com>
2026-04-08 17:38:42 +0900

    docker: Dockerfile, docker-compose.yml 설정

---
+++ src/main/java/springbootcode/code/basic/HealthController.java
@@ -0,0 +1,12 @@
+package springbootcode.code.basic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "ok";
+    }
+}
